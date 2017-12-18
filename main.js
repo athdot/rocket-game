b7cefd843c16dee30afa60da3d2ca88e36164750
athdot <athdot@gmail.com>
2017-12-18 09:39:59 -0700

    Update main.js

--- main.js
+++ main.js
@@ -504,7 +504,7 @@ function run() {
         d = new Date();
         currentMs = d.getMilliseconds();
         ratioMs = currentMs - (previousMs + 20);
-        speedMs -= ratioMs;
+        speedMs += ratioMs;
         clear();
         renderStars();
         if (place == 0) {
